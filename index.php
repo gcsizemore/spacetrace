d0c6a2b2c28c753bd2e96340193de6eb99798b61
Chris Wallace <cbwallace01@gmail.com>
2017-04-29 12:37:03 -0500

    Added Leaflet

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+        <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
+        <script src="js/index.js"></script>
     </head>
     <body>
         <div id='header'>
@@ -9,7 +12,7 @@
             <h5>Track Your Invasive Species</h5>
         </div>
         <div id='Content'>
-            
+            <div id='mapid'></div>
         </div>
         <div id='footer'>
             <p>Created By: Tim Sizemore, Geoff Sizemore, Brad West, & Chris Wallace</p>
