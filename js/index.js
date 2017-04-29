1b7562217a657b715b47e4785bfcd60e503f35cd
Chris Wallace <cbwallace01@gmail.com>
2017-04-29 13:32:27 -0500

    Commit

--- js/index.js
+++ js/index.js
@@ -14,7 +14,6 @@ window.onload = function(){
         var mymap = L.map('mapid').setView([position.coords.latitude, position.coords.longitude],13);
 
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
             maxZoom: 18,
             id: 'your.mapbox.project.id',
             accessToken: 'your.mapbox.public.access.token'
