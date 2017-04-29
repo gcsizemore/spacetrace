fda29a176b2e58103802544416d88bd8975febd4
Chris Wallace <cbwallace01@gmail.com>
2017-04-29 12:16:53 -0500

    Initial Webpage Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div id='header'>
+            <h3>Space Trace</h3>
+            <h5>Track Your Invasive Species</h5>
+        </div>
+        <div id='Content'>
+            
+        </div>
+        <div id='footer'>
+            <p>Created By: Tim Sizemore, Geoff Sizemore, Brad West, & Chris Wallace</p>
+        </div>
+    </body>
+</html>
